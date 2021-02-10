202057edcbed5b61e7830bd144049ec0d0d5c000
kory-job <catcher.intherye.rk@gmail.com>
2021-02-10 21:29:00 +0900

    画像投稿機能
    画像保存機能追加
    ルートを訂正

--- src/app/controllers/users_controller.rb
+++ src/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user, {only: [:new, :create]}
 
   def new
-    binding.pry
     @user = User.new(flash[:user])
   end
 
--- src/config/initializers/shrine.rb
+++ src/config/initializers/shrine.rb
@@ -3,7 +3,7 @@ require "shrine/storage/file_system"
 
 Shrine.storages = {
   cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), 
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       
+  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store"),       
 }
 
 Shrine.plugin :activerecord           
